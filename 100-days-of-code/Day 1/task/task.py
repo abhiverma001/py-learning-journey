cade29737f5c2e7db7c6ecf27ec7a3d30716b41b
abhiverma001 <ab.abhishekverma096@outlook.com>
2025-05-23 10:30:04 +0530

    first day yet to be completed

--- 100-days-of-code/Day 1/task/task.py
+++ 100-days-of-code/Day 1/task/task.py
@@ -1,4 +1,19 @@
-#taks-1
+#print something in console
+print("Hello world!")
 
-print ("Hellow World!")
+#string manipulations
+print("Hello world!")
+
+#or use '\n' between sentences or words for new line functions
+print("Hello world!\nHello world!\nHello world!")
+
+# combine two strings
+print()
+print ("Hello" + "Abhi")
+
+# add a space between words
+print()
+print("Hello" + " " + "Abhi")
+
+
 
