1d24ea917d9e929ae48e2cdcf913e9e4be7d1b0c
abhiverma001 <ab.abhishekverma096@outlook.com>
2025-05-25 00:49:00 +0530

    Day 1 Type Error, started

--- 100-days-of-code/Day 2/Data Types/task.py
+++ 100-days-of-code/Day 2/Data Types/task.py
@@ -1,1 +1,19 @@
-len("Hello")
+print("Hello"[0])  #Pulling a particular latter of a string, this is called subscripting
+print("Hello"[3])
+print("Hello"[-4])
+
+# String
+print("123" + "345")
+
+# Integer = Whole number
+print(123 + 345)  #This would be calculated, because we got actual number instead of string
+
+# Large Integers
+print(123_456_789)
+
+# Float = Floating Point Number
+print(3.12413)
+
+# Boolean
+print(True)
+print(False)
