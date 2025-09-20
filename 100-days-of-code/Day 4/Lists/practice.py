491b79a242d2538425a191367181b88eabf6df48
abhiverma001 <ab.abhishekverma096@outlook.com>
2025-09-21 00:30:04 +0530

    learnt list data structure

---
+++ 100-days-of-code/Day 4/Lists/practice.py
@@ -0,0 +1,16 @@
+#storing few states name of india in list
+states_of_india = ["Uttar Pradesh", "karnataka", "Andhra Pradesh", "Bihar"]
+print(states_of_india[1])    #in computer index numbers are start with 0 like uttar pradesh will be at 0 number, rest all similar way
+print(states_of_india[-1])   # - start taking from last
+
+states_of_india[0] = "UP"
+print(states_of_india)
+
+#using extend function to increase the list of state names
+states_of_india.extend(["Maharastra", "Goa"])
+print(states_of_india)
+
+#there are many other data structure function. ref the python official doc and do practice.
+
+
+
