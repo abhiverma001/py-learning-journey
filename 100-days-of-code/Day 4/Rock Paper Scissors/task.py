62a5999a718639e45a1b5afd39a14d86b89388f4
abhiverma001 <ab.abhishekverma096@outlook.com>
2025-09-21 01:39:19 +0530

    day 4 is completed

--- 100-days-of-code/Day 4/Rock Paper Scissors/task.py
+++ 100-days-of-code/Day 4/Rock Paper Scissors/task.py
@@ -1,3 +1,5 @@
+import random
+
 rock = '''
     _______
 ---'   ____)
@@ -24,3 +26,25 @@ scissors = '''
       (____)
 ---.__(___)
 '''
+
+
+image = [rock, paper, scissors]
+user_choice = int(input("What do you choose? Type 0 for rock, 1 for paper, 2 for scissors : "))
+if user_choice >= 0 and user_choice <= 2:
+    print(image[user_choice])
+
+computer_choice = random.randint(0, 2)
+print(image[computer_choice])
+#
+# if user_choice == computer_choice:
+#     print("retry")
+# elif user_choice > 2 or user_choice < 0 :
+#     print("invalid number!")
+# elif user_choice < computer_choice:
+#     print("You win!")
+# elif user_choice == 1 and 0 == computer_choice == 2 :
+#     print("You lose!")
+# elif user_choice == 2 and computer_choice == 1 :
+#     print("You win!")
+# elif user_choice == 2 and computer_choice == 0 :
+#     print("You loose!")
