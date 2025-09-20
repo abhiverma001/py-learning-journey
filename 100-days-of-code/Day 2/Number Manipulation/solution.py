b80fd988b9952c8f1938dc17c5e52bba0a3bb4b0
abhiverma001 <ab.abhishekverma096@outlook.com>
2025-09-20 17:45:51 +0530

    learning started

--- 100-days-of-code/Day 2/Number Manipulation/solution.py
+++ 100-days-of-code/Day 2/Number Manipulation/solution.py
@@ -30,5 +30,3 @@ height = 1.8
 is_winning = True
 
 print(f"Your score is = {score}, your height is {height}. You are winning is {is_winning}")
-
-
--- 100-days-of-code/Day 3/Modulo/task.py
+++ 100-days-of-code/Day 3/Modulo/task.py
@@ -4,3 +4,5 @@ if number_to_check % 2 == 0:
     print("Even")
 else:
     print("Odd")
+
+
