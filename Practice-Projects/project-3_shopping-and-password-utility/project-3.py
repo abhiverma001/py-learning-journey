fff8d8922aa21261349978e11d57f01880f51843
abhiverma001 <ab.abhishekverma096@outlook.com>
2025-09-24 01:31:59 +0530

    removed commented code

--- Practice-Projects/project-3_shopping-and-password-utility/project-3.py
+++ Practice-Projects/project-3_shopping-and-password-utility/project-3.py
@@ -2,32 +2,32 @@
 import random
 from xml.dom.minidom import ProcessingInstruction
 
-# print("\n Shopping List Manager")
-# shopping_list = []
-#
-# item1 = input("Enter the first item to add:")
-# shopping_list.append(item1)
-# print("Added:", item1)
-#
-# item2 = input("Enter the second item to add:")
-# shopping_list.append(item2)
-# print("Added:", item2)
-#
-# item3 = input("Enter the third item to add:")
-# shopping_list.append(item3)
-# print("Added:", item2)
-#
-# print("Current list of items:", shopping_list)
-#
-# #remove an item
-# remove_item = input("Enter an item to remove from list:")
-# if remove_item in shopping_list:
-#     shopping_list.remove(remove_item)
-#     print("Removed:", remove_item)
-# else:
-#     print("Item not found in shopping list")
-#
-# print("Current shopping list:", shopping_list)
+print("\n Shopping List Manager")
+shopping_list = []
+
+item1 = input("Enter the first item to add:")
+shopping_list.append(item1)
+print("Added:", item1)
+
+item2 = input("Enter the second item to add:")
+shopping_list.append(item2)
+print("Added:", item2)
+
+item3 = input("Enter the third item to add:")
+shopping_list.append(item3)
+print("Added:", item2)
+
+print("Current list of items:", shopping_list)
+
+#remove an item
+remove_item = input("Enter an item to remove from list:")
+if remove_item in shopping_list:
+    shopping_list.remove(remove_item)
+    print("Removed:", remove_item)
+else:
+    print("Item not found in shopping list")
+
+print("Current shopping list:", shopping_list)
 
 #Password generator
 print("\n Password Manager")
