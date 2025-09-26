4a7783120d271b85ecf7888368f2106293c41d45
abhiverma001 <ab.abhishekverma096@outlook.com>
2025-09-26 20:21:02 +0530

    day-6 is ongoing

---
+++ 100-days-of-code/Day 6/Functions/custom_function.py
@@ -0,0 +1,15 @@
+#check the total letters
+print("Hello")
+num_char = len("Hello")
+print(num_char)
+
+
+#To make our own function
+def my_function(): #our own custom function
+    print("Hello")
+    print("Abhishek")
+
+my_function()  #now calling our own custom function
+
+
+#while loop practice
---
+++ 100-days-of-code/Day 6/Functions/while_loop.py
@@ -0,0 +1,48 @@
+# for item in list_of_items:
+#   Do something to each item
+#
+# for number in range(a, b):
+#     print(number)
+
+
+# while something_is_true
+#     Do this
+#     Then do this
+#     Then do this
+
+
+# def turn_right():
+#     turn_left()
+#     turn_left()
+#     turn_left()
+#
+#
+# def jump():
+#     move()
+#     turn_left()
+#     move()
+#     turn_right()
+#     move()
+#     turn_right()
+#     move()
+#     turn_left()
+#
+# for flag in range(6):
+#    jump()
+#OR
+# number_of_hurdles = 6
+# while number_of_hurdles > 0:
+#     jump()
+#     number_of_hurdles -= 1
+#     print(number_of_hurdles)
+# OR
+# while not at_goal():
+#     jump()
+
+
+
+
+
+
+
+
