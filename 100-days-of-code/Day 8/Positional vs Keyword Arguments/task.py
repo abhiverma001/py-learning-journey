6d060c22f8ad34b29c7d946e805908db7841287d
abhiverma001 <ab.abhishekverma096@outlook.com>
2025-10-03 03:48:01 +0530

    day 8 is ongoing

--- 100-days-of-code/Day 8/Positional vs Keyword Arguments/task.py
+++ 100-days-of-code/Day 8/Positional vs Keyword Arguments/task.py
@@ -1,8 +1,40 @@
-# Functions with input
+# # Functions with input
 
-def greet_with_name(name):
-    print(f"Hello {name}")
-    print(f"How do you do {name}?")
+# def greet_with_name(name):
+#     print(f"Hello {name}")
+#     print(f"How do you do {name}?")
 
 
-greet_with_name("Jack Bauer")
+# greet_with_name("Jack Bauer")
+
+
+
+# # Functions with more than 1 input
+# def greet_with(name, location):
+#     print(f"Hello {name}")
+#     print(f"Happy to know you belong to my native plance : {location}")
+
+# name = input("please give your name :")
+# location = input("please provide you location :")
+
+# greet_with(name, location)
+
+
+
+
+#Love calc
+def calculate_love_score(name1, name2):
+    combine_name = (name1 + name2).lower()
+    true_count = 0
+    for letter in "true":
+        true_count += combine_name.count(letter)
+
+    love_count = 0
+    for letter in "love":
+        love_count += combine_name.count(letter)
+
+    love_score = int(f"{true_count}{love_count}")
+    print(f"Love Score = {love_score}")
+
+calculate_love_score("Kanye West", "Kim Kardashian")            
+
